68cb4ffe6e471c737394b73b1c219d5cb1d67ded
NicKarlen <nicolas.k@gmx.net>
2022-03-06 15:51:36 +0100

    more sqlite tutorial stuff

--- sqliteTutorial/rowID.py
+++ sqliteTutorial/rowID.py
@@ -6,8 +6,8 @@ conn = sqlite3.connect('database.db')
 # Create a cursor for db
 cursor = conn.cursor()
 
-# Read something in the database
-cursor.execute("SELECT * FROM orders")
+# rowid is a Primary Key which all rows have. We can see it if we select 'rowid'
+cursor.execute("SELECT rowid, * FROM orders")
 
 # fetch it from the db and print it to the console
 orders = cursor.fetchall()
