52f87545211d0372a78712dd722c0d1f40060e89
NicKarlen <nicolas.k@gmx.net>
2022-03-06 20:21:58 +0100

    get locally stored api key

--- Script_Nr1.py
+++ Script_Nr1.py
@@ -1,3 +1,19 @@
+from sre_compile import isstring
 from binance import Client
 import pandas as pd
-import os
+
+# function to get the api_keys from the locally safed file
+
+
+def getApiAccessKey(filename):
+    with open('API_Keys/'+filename) as f:
+        api_key, secret_key = next(f), next(f)
+
+    return {
+        "api_key": api_key[:-1],
+        "secret_key": secret_key
+    }
+
+
+print(getApiAccessKey('Binance_key.txt')['api_key'])
+print(getApiAccessKey('Binance_key.txt')['secret_key'])
