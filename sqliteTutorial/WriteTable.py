59fd889e38acfbdf71c5e80337cfbdf99f9147fd
NicKarlen <nicolas.k@gmx.net>
2022-03-06 15:12:21 +0100

    Write a table in sqlite

--- sqliteTutorial/WriteTable.py
+++ sqliteTutorial/WriteTable.py
@@ -1,21 +1,14 @@
 import sqlite3
 
-# sqlite is a fast light weight sql database, which comes with python. It doesn't need any extra service.
-# Connect to database 'database.db' or create one if it doesn't exist.
+# Connect to database 'database.db'
 conn = sqlite3.connect('database.db')
 
 # Create a cursor for db
 cursor = conn.cursor()
 
-# Create a table
-cursor.execute("""CREATE TABLE orders (
-    order_ID integer,
-    order_type text,
-    tradingpair text,
-    amount integer,
-    price integer,
-    time text
-    )""")
+# write into a table
+cursor.execute(
+    "INSERT INTO orders VALUES ('2','BUY','BTCUSD','100','38400','06.03.2022,15:08:58')")
 
 # DATATYPE for sqlite = NULL, INTEGER, REAL, TEXT, BLOB
 
