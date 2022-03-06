85c338979c25c9f0cdebdf429597e371a2a85a73
NicKarlen <nicolas.k@gmx.net>
2022-03-06 16:20:36 +0100

    clean up after SQLite tutorial

--- sqliteTutorial/1_CreateTable.py
+++ sqliteTutorial/1_CreateTable.py
@@ -2,6 +2,7 @@ import sqlite3
 
 # sqlite is a fast light weight sql database, which comes with python. It doesn't need any extra service.
 # Documentation can be found here: https://docs.python.org/3/library/sqlite3.html
+# Youtube tutorial I used is: https://www.youtube.com/watch?v=byHcYRpMgI4&t=884s
 # Connect to database 'database.db' or create one if it doesn't exist.
 conn = sqlite3.connect('database.db')
 
