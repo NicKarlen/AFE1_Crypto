be8dc5b555c5260e6d958e5360d30e53d10e2404
NicKarlen <nicolas.k@gmx.net>
2022-03-07 10:41:23 +0100

    First API call
    get 24h data from the ticker symbol BTCUSDT and creat a padas dataframe

--- Script_Nr1.py
+++ Script_Nr1.py
@@ -6,18 +6,27 @@ import pandas as pd
 
 def getApiAccessKey(filename):
     with open('API_Keys/'+filename) as f:
-        api_key, secret_key = next(f), next(f)
+        api_key, api_secret = next(f), next(f)
 
     return {
         "api_key": api_key[:-1],
-        "secret_key": secret_key
+        "api_secret": api_secret
     }
 
 
 BinanceApiKeys = getApiAccessKey('Binance_key.txt')
 
-print(BinanceApiKeys['api_key'])
-print(BinanceApiKeys['secret_key'])
+client = Client(BinanceApiKeys['api_key'], BinanceApiKeys['api_secret'])
+
+# print(BinanceApiKeys['api_key'])
+# print(BinanceApiKeys['api_secret'])
 
 # continue
 # https://youtu.be/_IV1qfSPPwI?t=318
+
+
+candles = client.get_ticker(symbol='BTCUSDT')
+
+candlespd = pd.DataFrame.from_dict(candles, orient='index')
+
+print(candlespd)
