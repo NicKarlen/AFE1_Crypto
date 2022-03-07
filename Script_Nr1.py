66ecb1052e27fb4f718aba1cce1c3e24e12c0c8a
NicKarlen <nicolas.k@gmx.net>
2022-03-07 14:15:31 +0100

    clean up

--- Script_Nr1.py
+++ Script_Nr1.py
@@ -21,9 +21,6 @@ BinanceApiKeys = getApiAccessKey('Binance_key.txt')
 
 client = Client(BinanceApiKeys['api_key'], BinanceApiKeys['api_secret'])
 
-# print(BinanceApiKeys['api_key'])
-# print(BinanceApiKeys['api_secret'])
-
 # continue
 # https://youtu.be/_IV1qfSPPwI?t=318
 
@@ -32,8 +29,6 @@ candles1min = client.get_historical_klines(
     "BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "1 minute ago UTC")
 print('Timestamp after API call:   ' + str(datetime.now()))
 
-# candles1min_pd = pd.DataFrame(columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time',
-#                                        'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
 
 candles1min_pd = pd.DataFrame({'Open time': candles1min[0][0], 'Open': candles1min[0][1], 'High': candles1min[0][2],
                                'Low': candles1min[0][3], 'Close': candles1min[0][4], 'Volume': candles1min[0][5],
@@ -45,6 +40,7 @@ candles1min_pd = pd.DataFrame({'Open time': candles1min[0][0], 'Open': candles1m
 print(candles1min_pd)
 
 # # Get 24h candle
+
 # candles = client.get_ticker(symbol='BTCUSDT')
 # candles_pd = pd.DataFrame.from_dict(candles, orient='index')
 # print(candles_pd)
