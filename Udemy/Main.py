c48920854a4d57783113f208ce72ba0d2ad0279e
NicKarlen <nicolas.k@gmx.net>
2022-03-21 13:09:45 +0100

    Getting all possible triangular pairs
    get all possible pair and save it as a json

--- Udemy/Main.py
+++ Udemy/Main.py
@@ -1,4 +1,5 @@
 import func_arb
+import json
 
 """
     Step 0: Finding coins which can be traded
@@ -16,16 +17,34 @@ def step_0():
     # Loop trough each objects and find the tradable pairs
     coin_list = func_arb.collect_tradeables(coin_json)
 
-    print(coin_list)
-    print(len(coin_json), len(coin_list))
+    # Print out the amout of all tradingpairs and the amount after checking if they are tradable
+    print("All the pairs that can be traded - ", "Full list: ",
+          len(coin_json), " Cleaned up list: ", len(coin_list))
 
+    # Return list of tradeable coins
     return coin_list
+
+
+"""
+    Step 1: Structuring Triangluar Pairs
+    Calculation only
+"""
+
+
+def step_1(coin_list):
+
+    # Structure the list of tradeable triangular arbitrage pairs
+    structured_list = func_arb.structure_triangular_pairs(coin_list)
+
+    # Save structured list
+    with open("Udemy/data/structured_triangular_pairs.json", "w") as fp:
+        json.dump(structured_list, fp)
 
 
 """ Main """
 if __name__ == "__main__":
     coin_list = step_0()
-
+    structured_pairs = step_1(coin_list)
     # step_1()
     # step_2()
     # step_3()
--- Udemy/func_arb.py
+++ Udemy/func_arb.py
@@ -12,6 +12,7 @@ def get_coin_tickers(url):
         return json_response
 
 
+# Loop through each object and find the tradable pairs
 def collect_tradeables(json_obj):
     coin_list = []
     for coin in json_obj:
@@ -20,3 +21,76 @@ def collect_tradeables(json_obj):
         if is_frozen == "0" and is_post_only == "0":
             coin_list.append(coin)
     return coin_list
+
+
+# Structure Arbitrage Pairs
+def structure_triangular_pairs(coin_list):
+
+    # Declare Variables:
+    triangular_pairs_list = []
+    remove_dublicates_list = []
+    pairs_list = coin_list[0:]
+
+    # Get Pair A
+    for pair_a in pairs_list:
+        pair_a_split = pair_a.split("_")
+        a_base = pair_a_split[0]
+        a_quote = pair_a_split[1]
+
+        # Assign A to a Box
+        a_pair_box = [a_base, a_quote]
+
+        # Get Pair B
+        for pair_b in pairs_list:
+            pair_b_split = pair_b.split("_")
+            b_base = pair_b_split[0]
+            b_quote = pair_b_split[1]
+
+            # Check Pair B
+            if pair_b != pair_a:
+                if b_base in a_pair_box or b_quote in a_pair_box:
+
+                    # Get Pair C
+                    for pair_c in pairs_list:
+                        pair_c_split = pair_c.split("_")
+                        c_base = pair_c_split[0]
+                        c_quote = pair_c_split[1]
+
+                        # Count the number of matching C items
+                        if pair_c != pair_a and pair_c != pair_b:
+                            combine_all = [pair_a, pair_b, pair_c]
+                            pair_box = [a_base, a_quote, b_base,
+                                        b_quote, c_base, c_quote]
+
+                            counts_c_base = 0
+                            for i in pair_box:
+                                if i == c_base:
+                                    counts_c_base += 1
+
+                            counts_c_quote = 0
+                            for i in pair_box:
+                                if i == c_quote:
+                                    counts_c_quote += 1
+
+                            # Determining Triangular Match
+                            if counts_c_base == 2 and counts_c_quote == 2 and c_base != c_quote:
+                                combined = pair_a + "," + pair_b + "," + pair_c
+                                unique_item = ''.join(sorted(combine_all))
+
+                                if unique_item not in remove_dublicates_list:
+                                    match_dict = {
+                                        "a_base": a_base,
+                                        "b_base": b_base,
+                                        "c_base": c_base,
+                                        "a_quote": a_quote,
+                                        "b_quote": b_quote,
+                                        "c_quote": c_quote,
+                                        "pair_a": pair_a,
+                                        "pair_b": pair_b,
+                                        "pair_c": pair_c,
+                                        "combined": combined
+                                    }
+                                    triangular_pairs_list.append(match_dict)
+                                    remove_dublicates_list.append(unique_item)
+
+    return triangular_pairs_list
